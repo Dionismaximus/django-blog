d1a2b69c2fc3d17282e34760e2ae1909d4920139
Denys Lytvynenko <dionismaximus666@gmail.com>
2024-09-26 19:55:13 +0000

    Solved the ModuleNotFoundIssue and finish about page

--- about/views.py
+++ about/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import About
 
 # Create your views here.
-def about(request):
+def about_me(request):
     about = About.objects.all().order_by('-updated_on').first()
 
     return render(
